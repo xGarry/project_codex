ecb7fa73d65355b12d11494de90fdf613e81998a
xGarry <46723163+xGarry@users.noreply.github.com>
2023-07-14 11:33:51 -0400

    first commit

---
+++ REST_API/server.js
@@ -0,0 +1,317 @@
+import express from 'express'
+import * as dotenv from'dotenv'
+import cors from 'cors'
+import bodyParser from 'body-parser';
+import { Configuration, OpenAIApi } from 'openai'
+import fetch from "node-fetch";
+import axios from "axios";
+import SibApiV3Sdk from "sib-api-v3-sdk";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  organization: process.env.ORG,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+//Get user queries on port 5000, send queries to chatGPT, get response and send it back to user.
+app.post('/', async (req, res) => {
+  try {
+    const { messages } = req.body; //Get user query
+    //initialize bot variables
+    const msgs = [{role: "system", content: "You are a helpful chatbot assistant for an e-commerce website that sells fragrance balms."},...messages];
+    const functions = [
+      {
+        name: "getProductInfo",
+        description: "Get relevant product information for all products such as product descriptions or product titles.",
+        parameters: {
+          type: "object",
+          properties: {
+            xfactor: {
+              type: "string",
+              description: 'The type of data required. E.g. "title" or "description"'
+            }
+          },
+          required: ["xfactor"],
+        }
+      },
+      {
+        name: "getOrderInfo",
+        description: "Get tracking link for a customer order using their order id and zip/postal code.",
+        parameters: {
+          type: "object",
+          properties: {
+            orderID: {
+              type: "string",
+              description: 'The order ID. E.g. "1234". Remove "#" if provided.'
+            },
+            zip: {
+              type: "string",
+              description: 'The customer zip or postal code for their shipping address. Must be entered without spaces.'
+            }
+          },
+          required: ["orderID", "zip"],
+        }
+      },
+      {
+        name: "getWeather",
+        description: "Gets the weather condition and location in the requester's local area.",
+        parameters: {
+          type: "object",
+          properties: {
+            weatherType: {
+              type: "string",
+              description: 'Type of weather data required e.g. "current"'
+            }
+          },
+          required: ["weatherType"],
+        }
+      },
+      {
+        name: "addSubscriber",
+        description: "Adds user as an email subscriber of emailing list.",
+        parameters: {
+          type: "object",
+          properties: {
+            fname: {
+              type: "string",
+              description: 'First name of user'
+            },
+            lname: {
+              type: "string",
+              description: 'Last name of user'
+            },
+            email: {
+              type: "string",
+              description: 'Email address of user'
+            }
+          },
+          required: ["fname", "lname", "email"],
+        }
+      }
+    ];
+    //Send user query to chatGPT and wait for response
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo-0613",
+      messages: msgs,
+      functions: functions,
+      function_call: "auto",
+    });
+    //Store chatGPT's response
+    const completionResponse = completion.data.choices[0].message;
+    
+    if(!completionResponse.content) //If user query requires calling a function
+    {
+      const functionCallName = completionResponse.function_call.name; //Then get function name
+
+      if(functionCallName === "getProductInfo") //If function is getProductInfo
+      { 
+        const completionArguments = JSON.parse(completionResponse.function_call.arguments); //get function arguments from GPT
+
+        try 
+        {
+          const productInfo = await getProductInfo(completionArguments.xfactor); //Call the function with given arguments
+          //console.log(completionArguments.xfactor);
+          msgs.push(completionResponse); //Add intitial response to message history
+          msgs.push({ role: 'function', name: functionCallName, content: `${productInfo}` }); //add function results to message history
+        } 
+        catch (error) { console.error(error);}
+      }   
+
+      if(functionCallName === "getOrderInfo") //If function is getOrderInfo
+      { 
+        const completionArguments = JSON.parse(completionResponse.function_call.arguments);
+        
+        try {
+          const orderInfo = await getOrderInfo(completionArguments.orderID, completionArguments.zip);
+          console.log(completionArguments.orderID);
+          msgs.push(completionResponse);
+          msgs.push({ role: 'function', name: functionCallName, content: `${orderInfo}` });
+        } 
+        catch (error) { console.error(error);}
+      }
+
+      if(functionCallName === "getWeather") //If function is getWeather
+      { 
+        //const completionArguments = JSON.parse(completionResponse.function_call.arguments);
+        try {
+          const weather = await getWeather(req.ip);
+          console.log(req.ip);
+          msgs.push(completionResponse);
+          msgs.push({ role: 'function', name: functionCallName, content: `${weather}` });
+        } 
+        catch (error) { console.error(error);}
+      }
+
+      if(functionCallName === "addSubscriber") //If function is addSubscriber
+      { 
+        const completionArguments = JSON.parse(completionResponse.function_call.arguments);
+        
+        try {
+          const subscriberInfo = addSubscriber(completionArguments.fname, completionArguments.lname, completionArguments.email);
+          msgs.push(completionResponse);
+          msgs.push({ role: 'function', name: functionCallName, content: `${subscriberInfo}` });
+        } 
+        catch (error) { console.error(error);}
+      }
+      //Send function results to chatGPT and await final response
+      const second_response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo-0613",
+        messages: msgs,
+      });
+      //Get final response from chatGPT and send it to user
+      res.json({
+        completion: second_response.data.choices[0].message
+      });
+
+    }
+    //Else function call is not required
+    else
+    {
+      res.json({
+        completion: completionResponse //send user the initial response from chatGPT
+      });
+    }
+  } 
+  catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+
+})
+
+//Listen for user queries on port 5000
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
+
+//------------------------------------FUNCTIONS------------------------------------------//
+let shopifyKey = process.env.SHOPIFY_API_KEY;
+let pass = process.env.PASSWORD;
+
+//Get product information from Shopify
+async function getProductInfo(xfactor) 
+{
+  let endPoint = "products";
+  try {
+    let options = {
+      "method": "GET",
+      "url": `https://${shopifyKey}:${pass}@dabalmdotcom-8456.myshopify.com/admin/api/2022-04/${endPoint}.json`,
+      "headers":  {
+          "Content-Type": "application/json"
+      }
+  };
+
+    const response = await axios(options);
+    const data = response.data;
+    console.log(data);
+    const obj = result(data, xfactor);
+    return obj;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+//Parse product information and return relevant details
+function result(data, xfactor) 
+{
+  const finddata = [];
+  
+  for (const item in data['products']) 
+  {
+    if(xfactor === "description")
+    {
+      finddata.push(data['products'][item].body_html);
+    }
+    else if(xfactor === "title")
+    {
+      finddata.push(data['products'][item].title);
+    }
+  }
+  return finddata.toString();
+}
+
+//Get order information from Shopify
+async function getOrderInfo(orderID, zip) 
+{
+  let endPoint = "orders";
+  try {
+    let options = {
+      "method": "GET",
+      "url": `https://${shopifyKey}:${pass}@dabalmdotcom-8456.myshopify.com/admin/api/2022-04/${endPoint}.json?name=%23${orderID}&status=any`,
+      "headers":  {
+          "Content-Type": "application/json"
+      }
+  };
+    const response = await axios(options);
+    const data = response.data;
+    console.log("data: ", data["orders"][0]);
+    const obj = data["orders"][0].order_status_url.toString();
+    let ActualZip = data["orders"][0].shipping_address.zip.toString();
+
+    ActualZip = ActualZip.toLowerCase().replace(/\s/g, "");
+    zip = zip.toLowerCase();
+    console.log("zip: ", zip);
+    console.log("ActualZip: ", ActualZip);
+
+    if(ActualZip === zip){
+      return obj;
+    }else{
+      console.log("Incorrect zip/postal code");
+      return "Incorrect zip/postal code";
+    }
+       
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+async function getWeather(ip) {
+  try {
+    const options = {
+      method: 'GET',
+      url: `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${ip}`,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+
+    const response = await axios(options);
+    const data = response.data;
+
+    const condition = "location: " + data.location.name.toString() + ", weather: " + data.current.condition.text.toString();
+    console.log(condition);
+    return condition;
+
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+function addSubscriber(fname, lname, email){
+  let defaultClient = SibApiV3Sdk.ApiClient.instance;
+  let brevoKey = defaultClient.authentications['api-key'];
+  brevoKey.apiKey = process.env.BREVO_API_KEY;
+  let apiInstance = new SibApiV3Sdk.ContactsApi();
+  let createContact = new SibApiV3Sdk.CreateContact();
+  
+  createContact.email = email;
+  createContact.attributes = {"FIRSTNAME":fname,"LASTNAME":lname};
+  createContact.listIds = [4]
+  
+  apiInstance.createContact(createContact).then(function(data) {
+    console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+    return('API called successfully. Returned data: ' + JSON.stringify(data));
+  }, function(error) {
+    console.error(error);
+    return error;
+  });
+}
+  
---
+++ client/chatbot.js
@@ -0,0 +1,115 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+let userMessage = "Use current weather and location data say precisely the following: Hi there 👋 [make a new line] it looks like a [based on current weather say either: cloudy/rainy/sunny/snowy/windy/foggy] day in [location]. How can I help you today? If you can't get the weather data simply say: Hi there 👋 [make a new line] How can I help you today?"; // Variable to store user's message
+let messages = [];
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+    // Create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", `${className}`);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
+}
+
+const generateResponse = (chatElement) => {
+    const API_URL = "https://codex-a153.onrender.com";
+    const messageElement = chatElement.querySelector("p");
+    const newMessage = {"role": "user", "content": `${userMessage}`};
+    messages.push(newMessage);
+
+    // Define the properties and message for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            messages
+        })
+    }
+
+    // Send POST request to API, get response and set the reponse as paragraph text
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messages.push({"role": "assistant", "content": `${data.completion.content.trim()}`});
+        messageElement.textContent = data.completion.content.trim();
+        
+        var currentText = messageElement.innerHTML;
+        // Regular expression to match a URL
+        var urlRegex = /\((https?:\/\/[^\s]+)\)/;
+        
+        // Check if the current text contains a URL
+        if (urlRegex.test(currentText)) {
+            var match = currentText.match(urlRegex);
+            var url = match ? match[1] : null;
+            // Replace the URL with a blank
+            var newText = currentText.replace(urlRegex, '');
+            var trackOrderRegex = /\[(.*?)\]/g;
+            newText = newText.replace(trackOrderRegex, '<a href="' + url + '" target="_blank">[Track Order]</a>');
+            // Update the content of the <p> element
+            messageElement.innerHTML = newText;
+        }
+
+    }).catch(() => {
+        messageElement.classList.add("error");
+        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+    if(!userMessage) return;
+
+    // Clear the input textarea and set its height to default
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    
+    setTimeout(() => {
+        // Display "Thinking..." message while waiting for the response
+        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+function intializeChat(){
+    setTimeout(() => {
+        // Display "Thinking..." message while waiting for the response
+        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+intializeChat();
+
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key and the window 
+    // width is greater than 800px, handle the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
