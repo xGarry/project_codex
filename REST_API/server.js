b2729a7d2a3a680c0ba0543bf99f2f62b065a067
xGarry <46723163+xGarry@users.noreply.github.com>
2023-07-20 12:11:32 -0400

    undid log change

--- REST_API/server.js
+++ REST_API/server.js
@@ -24,7 +24,6 @@ app.use(cors())
 app.post('/', async (req, res) => {
   try {
     const { messages } = req.body; //Get user query
-    saveChatLogs(req.body);
     //initialize bot variables
     const msgs = [{role: "system", content: "You are a helpful chatbot assistant for an e-commerce website that sells fragrance balms. If you are unable to answer a prompt, ask the customer to contact dabalmdotcom@gmail.com"},...messages];
     const functions = [
